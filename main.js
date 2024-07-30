99e79498c4f3b3fbeb8abba2b7b3171be70e5383
spstefan <stefansp@uio.no>
2024-07-30 21:18:45 +0200

    refactor navigation bar

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-const tabsContainer = document.querySelector("#tabs-container");
-const tabsList = tabsContainer.querySelector(".tabs-list");
-const tabButtons = tabsList.querySelectorAll(".tab-button");
-const tabPanels = tabsContainer.querySelectorAll(".tab__panels > div");
+const navBar = document.querySelector(".nav-bar");
+const tabButtons = navBar.querySelectorAll(".tab-button");
+const tabContent = document.querySelectorAll(".tab-content > div");
 
 /* Hides all but the first tab in the content box */
 tabButtons.forEach((tab, index) => {
     if (index === 0) {
         tab.classList.add('active');
     } else {
-        tabPanels[index].setAttribute("hidden", "");
+        tabContent[index].setAttribute("hidden", "");
     }
 });
 
 /* On click reveals tab contents and hides the others */
-tabsContainer.addEventListener("click", (e) => {
+navBar.addEventListener("click", (e) => {
     const clickedTab = e.target.closest("a");
     if (!clickedTab) return;
     e.preventDefault();
 
     const activePanelId = clickedTab.getAttribute('href');
-    const activePanel = tabsContainer.querySelector(activePanelId);
+    const activePanel = document.querySelector(activePanelId);
 
-    tabPanels.forEach((panel => {
+    tabContent.forEach((panel => {
         panel.setAttribute("hidden", true);
     }))
     activePanel.removeAttribute("hidden");
