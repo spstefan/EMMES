0a906eae6ef0e5ed6c9f00167e83e1c89d5e0189
spstefan <stefansp@uio.no>
2024-07-31 22:02:52 +0200

    add half-working task list

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* ---- NAVIGATION IN CONTENT BOX ---- */
 const navBar = document.querySelector(".nav-bar");
 const tabButtons = navBar.querySelectorAll(".tab-button");
 const tabContent = document.querySelectorAll(".tab-content > div");
@@ -11,6 +12,9 @@ tabButtons.forEach((tab, index) => {
     }
 });
 
+
+var activeContentId = "#daily"; 
+
 /* On click reveals tab contents and hides the others */
 navBar.addEventListener("click", (e) => {
     const clickedTab = e.target.closest("a");
@@ -18,6 +22,7 @@ navBar.addEventListener("click", (e) => {
     e.preventDefault();
 
     const activePanelId = clickedTab.getAttribute('href');
+    activeContentId = activePanelId;
     const activePanel = document.querySelector(activePanelId);
 
     tabContent.forEach((panel => {
@@ -31,3 +36,65 @@ navBar.addEventListener("click", (e) => {
 
     clickedTab.classList.add('active');
 } )
+
+/* ---- TASK LISTS ---- */
+
+function initializeTaskList(activeContentId) {
+    const activeContent = document.getElementById(activeContentId.slice(1));
+    const taskList = activeContent.querySelectorAll(`ul${activeContentId}-tasks li`);
+
+    var i;
+    for (i = 0; i < taskList.length; i++) {
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        taskList[i].appendChild(span);
+    }
+
+    var close = document.getElementsByClassName("close");
+    var i;
+    for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+    }
+
+    var list = activeContent.querySelector(`ul${activeContentId}-tasks`);
+
+    list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        console.log(ev.target);
+        ev.target.classList.toggle('checked');
+        }
+    }, false);
+}
+
+function newElement() {
+  var li = list.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = list.createTextNode(inputValue);
+  li.appendChild(t)
+  if (inputValue === '') {
+    alert("Input can not be empty!");
+  } else {
+    list.appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }   
+} 
+
+initializeTaskList(activeContentId);
